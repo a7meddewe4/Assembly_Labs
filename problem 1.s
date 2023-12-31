9ff91457932ecb209c83cc5013b5beab0b4a62fa
a7meddewe4 <121269309+a7meddewe4@users.noreply.github.com>
2023-12-31 06:15:14 +0200

    Update problem 1.s

--- problem 1.s
+++ problem 1.s
@@ -1,68 +1,35 @@
-myArray:
-    DB 2
-    DB 1
-    DB 2
-    DB 3
-    DB 4
-    DB 5
-    DB 6
-    DB 7
-    DB 8
-    DB 9
-    DB 10
-    DB 11
-    DB 12
-    DB 13
-    DB 14
-    DB 15
-    DB 16
-    DB 17
-    DB 18
-    DB 19
-    DB 20
-    DB 21
-    DB 22
-    DB 23
-    DB 24
-    DB 25
-    DB 26
-    DB 27
-    DB 28
-    DB 29
-    DB 30
-; resultarray: DB 0
-HANDRED: dw 100
-FIN: dw 30
-half: dw 15
+myArray: 
+db 20
+db 21
+db 22
+db 23
+db 24
+length: db 0
+thirty:dw 30
+hundred:dw 100
+half:dw 15
 start:
-MOV CH,0 ; counter 
-MOV SI,OFFSET myArray
-; MOV DI, OFFSET resultarray
-
-calc_percentage:
-mov dx, 0
-
-CMP CH,31
-JA  end_code
-MOV AL, BYTE [SI]
-MUL  word HANDRED
-DIV  word FIN
-MOV byte [SI], AL
-CMP DX, word half
-jge increas
-continue:
-INC CH
-INC SI
-; INC DI
-MOV AX,0
-CLC
-JMP calc_percentage
-increas:
-    inc al
-    mov byte ds[si],al
-    jmp continue
-    
-    
-
-end_code:
-INT 3
+mov si,OFFSET length
+mov cx,si
+add cx,1
+mov si,0
+st:
+loop make_percentage
+ja end
+make_percentage:
+mov al,1
+mul byte [si]
+mul word hundred
+div word thirty
+cmp dx,word half
+jge addone
+mov byte[si],al
+add si,1
+jmp st
+addone:
+add al,1
+mov byte[si],al
+add si,1
+jmp st
+end:
+int 3
